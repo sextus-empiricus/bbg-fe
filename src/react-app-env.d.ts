454af6b52b01e6ca82ad6a5ab27f44e272cbe33e
Sextus Empiricus <8272331@gmail.com>
2022-10-06 08:09:34 +0200

    basic layout created

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,3 +1,2 @@
 /*eslint-disable*/
 /// <reference types="react-scripts" />
-
--- src/views/Layout.view.tsx
+++ src/views/Layout.view.tsx
@@ -1,17 +1,9 @@
 import React, { ReactElement } from 'react';
 
-import { SignInForm } from '../components/auth/SingInForm';
-import { CenterContainer } from '../components/common/CenterContainer';
 import { MainLayout } from '../components/layouts/Main/Main.layout';
 
 const LayoutView = (): ReactElement => {
-   return (
-      <MainLayout>
-         <CenterContainer>
-            <SignInForm />
-         </CenterContainer>
-      </MainLayout>
-   );
+   return <MainLayout />;
 };
 
 export { LayoutView };
