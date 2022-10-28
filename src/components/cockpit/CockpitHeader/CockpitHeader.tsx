c11ed0ef065c66a81153df77643c3ad5a1839dc6
Sextus Empiricus <8272331@gmail.com>
2022-10-28 07:00:42 +0200

    CockpitHeader responsiveness fixed

--- src/components/cockpit/CockpitHeader/CockpitHeader.tsx
+++ src/components/cockpit/CockpitHeader/CockpitHeader.tsx
@@ -1,13 +1,15 @@
 import React, { ReactElement } from 'react';
 import { Box, Typography } from '@mui/material';
 
-import { LiveClock } from '../common/LiveClock';
+import { LiveClock } from '../../common/LiveClock';
+
+import classes from './CockpitHeader.module.scss';
 
 const CockpitHeader = (): ReactElement => {
    return (
       <Box display='flex' justifyContent='space-between' sx={{ padding: '20px' }}>
-         <Box>
-            <Typography variant='h4' component='h2' sx={{ fontSize: '2rem' }}>
+         <Box display='flex' justifyContent='center' alignItems='center'>
+            <Typography variant='h4' component='h2' sx={{ fontSize: { xs: '1rem', sm: '2rem' } }}>
                Active Trades:
             </Typography>
          </Box>
@@ -22,9 +24,7 @@ const CockpitHeader = (): ReactElement => {
             >
                {new Date().toLocaleDateString()}
             </Typography>
-            <LiveClock
-               style={{ fontWeight: 300, fontSize: '1.3rem', color: 'rgba(255,255,255, 0.75)' }}
-            />
+            <LiveClock className={classes.LiveClock} />
          </Box>
       </Box>
    );
